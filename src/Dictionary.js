d14cc3db0985853a0e6d1d772ebf6bf7507cfd61
Tanaz-kh-ne <tanaz.kh.ne@gmail.com>
2022-07-27 19:37:06 +0100

    Installed Axios, added Dictionary API URL and added axios call function
    Created useState data, setData

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,15 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [data, setData] = useState("");
+
+  function handleResponse(response) {
+    setData(response.data[0]);
+  }
 
   function keywordSearch(event) {
     setKeyword(event.target.value);
@@ -11,6 +17,8 @@ export default function Dictionary() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
